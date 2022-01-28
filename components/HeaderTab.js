fc54b502e587882fa38a643327fca679894f848d
moneshkannan <moneshkannan02@gmail.com>
2022-01-28 21:59:47 +0530

    first commit

---
+++ components/HeaderTab.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function HeaderTab() {
+  return (
+    <View>
+      <Text>Header Tab</Text>
+    </View>
+  );
+}
